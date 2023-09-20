5ffb0528b1f17d0d95b97e5a0d067548d84e7897
User A0657865 <a0657865@greip.from.sh>
2023-09-20 11:16:23 +0300

    edit

--- FoodBot/Bot/Settings.php
+++ FoodBot/Bot/Settings.php
@@ -5,11 +5,7 @@ class Settings
 {
     private const SETTINGS = [
         "HOST" => "localhost",
-        "USER_NAME" => "a0657865_foodPlan",
-        "PASSWORD" => "0lPvxzG9",
-        "DB_NAME" => "a0657865_foodPlan",
         "MAX_PROTEIN" => 55,
-        "TOKEN" => "6169354773:AAGjAa7TeoBhG9e4sOUN-oCLLLIQLNkOXFQ",
     ];
 
     public function __get(string $key)
